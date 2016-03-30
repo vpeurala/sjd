3f3716c8ae15b99bc9f64230c7d4efcafa7021b1
Ville Peurala <ville.peurala@gmail.com>
2016-03-30 20:48:06 +0300

    Fix compiler warnings

--- src/Parser.hs
+++ src/Parser.hs
@@ -1,16 +1,15 @@
+{-# LANGUAGE RankNTypes #-}
 module Parser where
 
 import Model
-
 import Text.ParserCombinators.Parsec
-
 import Data.Maybe (fromMaybe)
 
 type FileContent = String
 
 data FieldDeclaration = FieldDeclaration FieldName ClassName deriving (Show)
 
-data PackageOrClassDeclaration = OfPackageDeclaration PackageDeclaration | 
+data PackageOrClassDeclaration = OfPackageDeclaration PackageDeclaration |
                                  OfClassDeclaration ClassDeclaration deriving (Show)
 
 data PackageDeclaration = PackageDeclaration PackageName deriving (Show)
@@ -21,25 +20,26 @@ data ClassDeclaration = ClassDeclaration ClassName Implements [FieldDeclaration]
 data CodebaseDeclaration = CodebaseDeclaration [PackageOrClassDeclaration] deriving (Show)
 
 fieldName :: GenParser Char st ClassName
-fieldName = many1 (oneOf (['A'..'Z'] ++ ['a'..'z'] ++ ['0'..'9'] ++ "_"))
+fieldName = many1 . oneOf $ mconcat [['A'..'Z'], ['a'..'z'], ['0'..'9'], "_"]
 
 className :: GenParser Char st ClassName
-className = many1 (oneOf (['A'..'Z'] ++ ['a'..'z'] ++ ['0'..'9'] ++ "._"))
+className = many1 . oneOf $ mconcat [['A'..'Z'], ['a'..'z'], ['0'..'9'], "._"]
 
 typeName :: GenParser Char st TypeName
-typeName = many1 (oneOf (['A'..'Z'] ++ ['a'..'z'] ++ ['0'..'9'] ++ "._<>"))
+typeName = many1 . oneOf $ mconcat [['A'..'Z'], ['a'..'z'], ['0'..'9'], "._<>"]
 
 packageName :: GenParser Char st PackageName
-packageName = many1 (oneOf (['A'..'Z'] ++ ['a'..'z'] ++ ['0'..'9'] ++ "._"))
+packageName = many1 . oneOf $ mconcat [['A'..'Z'], ['a'..'z'], ['0'..'9'], "._"]
 
 -- TODO vpeurala 5.11.2015: Take primitives and other special cases into account
+fieldType :: forall st. GenParser Char st TypeName
 fieldType = typeName
 
 fieldDeclaration :: GenParser Char st FieldDeclaration
 fieldDeclaration = do
     fieldName' <- fieldName
     spaces
-    string ":"
+    _ <- string ":"
     spaces
     fieldType' <- fieldType
     spaces
@@ -47,7 +47,7 @@ fieldDeclaration = do
 
 implements :: GenParser Char st Implements
 implements = do
-    string "implements "
+    _ <- string "implements "
     spaces
     types <- many1 typeName
     spaces
@@ -55,7 +55,7 @@ implements = do
 
 classDeclaration :: GenParser Char st PackageOrClassDeclaration
 classDeclaration = do
-    string "class "
+    _ <- string "class "
     spaces
     className' <- className
     spaces
@@ -64,15 +64,15 @@ classDeclaration = do
     spaces
     fields <- many (try fieldDeclaration)
     spaces
-    return $ OfClassDeclaration $ ClassDeclaration className' implements'' fields
+    return . OfClassDeclaration $ ClassDeclaration className' implements'' fields
 
 packageDeclaration :: GenParser Char st PackageOrClassDeclaration
 packageDeclaration = do
-    string "package "
+    _ <- string "package "
     spaces
     packageName' <- packageName
     spaces
-    return $ OfPackageDeclaration $ PackageDeclaration packageName'
+    return . OfPackageDeclaration $ PackageDeclaration packageName'
 
 packageOrClassDeclaration :: GenParser Char st PackageOrClassDeclaration
 packageOrClassDeclaration =
