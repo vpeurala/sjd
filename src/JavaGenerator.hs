068214e31b703dceed1f3ba3c85c89804d213318
Ville Peurala <ville.peurala@reaktor.fi>
2016-04-05 11:52:00 +0300

    Use qualified imports on JavaGenerator

--- src/JavaGenerator.hs
+++ src/JavaGenerator.hs
@@ -4,116 +4,116 @@ import Control.Monad.Reader
 import Data.List (intercalate)
 
 import qualified JavaCommon as J
-import Model as M
-import Util as U
+import qualified Model as M
+import qualified Util as U
 
-generateJavaClass :: J.Generator -> M.Codebase -> M.Package -> M.Class -> JavaSource
+generateJavaClass :: J.Generator -> M.Codebase -> M.Package -> M.Class -> M.JavaSource
 generateJavaClass generator codebase package@(M.Package (Just packageName) _) klass@(M.Class _ className maybeExtends implements fields) =
-  JavaSource (packageName ++ "." ++ className) sourceCode
+  M.JavaSource (packageName ++ "." ++ className) sourceCode
   where sourceCode = packageDeclaration packageName ++
                      runReader J.importDeclarations (J.ClassReaderEnv generator codebase package klass) ++
                      "\n" ++
                      classDeclaration className maybeExtends implements ++
-                     indent (fieldDeclarations fields) ++
+                     U.indent (fieldDeclarations fields) ++
                      "\n" ++
-                     indent (constructorDeclaration className fields) ++
-                     indent (getters fields) ++
+                     U.indent (constructorDeclaration className fields) ++
+                     U.indent (getters fields) ++
                      "\n" ++
-                     indent (toString className fields) ++
+                     U.indent (toString className fields) ++
                      "\n" ++
-                     indent (equals className fields) ++
+                     U.indent (equals className fields) ++
                      "\n" ++
-                     indent (hashCode fields) ++
+                     U.indent (hashCode fields) ++
                      "}"
 generateJavaClass generator codebase package@(M.Package Nothing _) klass@(M.Class _ className maybeExtends implements fields) =
-  JavaSource className sourceCode
+  M.JavaSource className sourceCode
   where sourceCode = runReader J.importDeclarations (J.ClassReaderEnv generator codebase package klass) ++
                      "\n" ++
                      classDeclaration className maybeExtends implements ++
-                     indent (fieldDeclarations fields) ++
+                     U.indent (fieldDeclarations fields) ++
                      "\n" ++
-                     indent (constructorDeclaration className fields) ++
-                     indent (getters fields) ++
+                     U.indent (constructorDeclaration className fields) ++
+                     U.indent (getters fields) ++
                      "\n" ++
-                     indent (toString className fields) ++
+                     U.indent (toString className fields) ++
                      "\n" ++
-                     indent (equals className fields) ++
+                     U.indent (equals className fields) ++
                      "\n" ++
-                     indent (hashCode fields) ++
+                     U.indent (hashCode fields) ++
                      "}"
 
-packageDeclaration :: PackageName -> SourceCode
+packageDeclaration :: M.PackageName -> M.SourceCode
 packageDeclaration packageName = "package " ++ packageName ++ ";\n\n"
 
-importsFromFieldType :: M.FieldType -> J.ClassReader [Import]
+importsFromFieldType :: M.FieldType -> J.ClassReader [M.Import]
 importsFromFieldType fieldType = do
   needsImport <- J.needsImport fieldType
   if needsImport
     then J.fqns fieldType
     else case fieldType of
-      List _              -> return ["java.util.List"]
-      Optional fieldType' -> do
+      M.List _              -> return ["java.util.List"]
+      M.Optional fieldType' -> do
         importsFromFieldType' <- importsFromFieldType fieldType'
         return $ "java.util.Optional" : importsFromFieldType'
-      Object className    -> return $ case className of
+      M.Object className    -> return $ case className of
         "LocalDate"                       -> ["java.time.LocalDate"]
         "LocalDateTime"                   -> ["java.time.LocalDateTime"]
         _                                 -> []
       _                   -> return []
 
-classDeclaration :: ClassName -> Maybe Extends -> Implements -> SourceCode
+classDeclaration :: M.ClassName -> Maybe M.Extends -> M.Implements -> M.SourceCode
 classDeclaration className extends implements = "public class " ++ className ++ " " ++
   extendsDeclaration extends ++
   implementsDeclaration implements ++
   "{\n"
 
-extendsDeclaration :: Maybe Extends -> SourceCode
+extendsDeclaration :: Maybe M.Extends -> M.SourceCode
 extendsDeclaration e = case e of
   Nothing  -> ""
   (Just s) -> "extends " ++ s ++ " "
 
-implementsDeclaration :: Implements -> SourceCode
+implementsDeclaration :: M.Implements -> M.SourceCode
 implementsDeclaration implements = if null implements
   then ""
   else "implements " ++ intercalate ", " implements ++ " "
 
-fieldDeclarations :: [M.Field] -> SourceCode
+fieldDeclarations :: [M.Field] -> M.SourceCode
 fieldDeclarations = concatMap fieldDeclaration
 
-fieldDeclaration :: M.Field -> SourceCode
+fieldDeclaration :: M.Field -> M.SourceCode
 fieldDeclaration (M.Field fieldName fieldType) = "private final " ++ J.javaize fieldType ++ " " ++ fieldName ++ ";\n"
 
-constructorDeclaration :: ClassName -> [M.Field] -> SourceCode
+constructorDeclaration :: M.ClassName -> [M.Field] -> M.SourceCode
 constructorDeclaration className fields =
   "@JsonCreator\n" ++
   "public " ++ className ++ "(" ++ intercalate (",\n" ++ replicate (length ("public " ++ className ++ "(")) ' ') (map (\(M.Field fieldName fieldType) -> "@JsonProperty(\"" ++ fieldName ++ "\") " ++ J.javaize fieldType ++ " " ++ fieldName) fields) ++ ") {\n" ++
-  indent (concatMap (\(M.Field fieldName _) -> "Objects.requireNonNull(" ++ fieldName ++ ", \"Property '" ++ fieldName ++ "' cannot be null.\");\n") fields) ++
-  indent (concatMap (\(M.Field fieldName _) -> "this." ++ fieldName ++ " = " ++ fieldName ++ ";\n") fields) ++
+  U.indent (concatMap (\(M.Field fieldName _) -> "Objects.requireNonNull(" ++ fieldName ++ ", \"Property '" ++ fieldName ++ "' cannot be null.\");\n") fields) ++
+  U.indent (concatMap (\(M.Field fieldName _) -> "this." ++ fieldName ++ " = " ++ fieldName ++ ";\n") fields) ++
   "}\n\n"
 
-getters :: [M.Field] -> SourceCode
+getters :: [M.Field] -> M.SourceCode
 getters fields = intercalate "\n" (map getter fields)
 
-getter :: M.Field -> SourceCode
-getter (M.Field fieldName fieldType) = "public " ++ J.javaize fieldType ++ " " ++ getOrIs fieldType ++ upcase fieldName ++ "() {\n" ++
-  indent ("return " ++ fieldName ++ ";") ++
+getter :: M.Field -> M.SourceCode
+getter (M.Field fieldName fieldType) = "public " ++ J.javaize fieldType ++ " " ++ getOrIs fieldType ++ U.upcase fieldName ++ "() {\n" ++
+  U.indent ("return " ++ fieldName ++ ";") ++
   "}\n"
 
-getOrIs :: M.FieldType -> SourceCode
+getOrIs :: M.FieldType -> M.SourceCode
 getOrIs fieldType = case fieldType of
-  Boolean -> "is"
+  M.Boolean -> "is"
   _       -> "get"
 
-toString :: ClassName -> [M.Field] -> SourceCode
+toString :: M.ClassName -> [M.Field] -> M.SourceCode
 toString className fields = "@Override\npublic String toString() {\n" ++
-  indent ("return \"" ++ className ++ "@\" + System.identityHashCode(this) + \": {\"\n" ++
-    indent (concatMap (\(M.Field fieldName _) -> "+ \"" ++ fieldName ++ " = '\" + " ++ fieldName ++ " + \"'\"\n") fields) ++
+  U.indent ("return \"" ++ className ++ "@\" + System.identityHashCode(this) + \": {\"\n" ++
+    U.indent (concatMap (\(M.Field fieldName _) -> "+ \"" ++ fieldName ++ " = '\" + " ++ fieldName ++ " + \"'\"\n") fields) ++
     ";\n") ++
   "}\n"
 
-equals :: ClassName -> [M.Field] -> SourceCode
+equals :: M.ClassName -> [M.Field] -> M.SourceCode
 equals className fields = "@Override\npublic boolean equals(Object o) {\n" ++
-  indent (
+  U.indent (
     "if (this == o) return true;\n" ++
     "if (o == null) return false;\n" ++
     "if (this.getClass() != o.getClass()) return false;\n" ++
@@ -129,21 +129,21 @@ equals className fields = "@Override\npublic boolean equals(Object o) {\n" ++
   ) ++
   "}\n"
 
-hashCode :: [M.Field] -> SourceCode
+hashCode :: [M.Field] -> M.SourceCode
 hashCode fields = "@Override\npublic int hashCode() {\n" ++
-  indent (
+  U.indent (
     "int result = 0;\n" ++
     concatMap (
       \(M.Field fieldName fieldType) -> case fieldType of
-        Long                        -> "result = 31 * Long.hashCode(" ++ fieldName ++ ");\n"
-        Float                       -> "result = 31 * Float.hashCode(" ++ fieldName ++ ");\n"
-        Double                      -> "result = 31 * Double.hashCode(" ++ fieldName ++ ");\n"
-        Boolean                     -> "result = 31 * result + (" ++ fieldName ++ " ? 1 : 0);\n"
+        M.Long                        -> "result = 31 * Long.hashCode(" ++ fieldName ++ ");\n"
+        M.Float                       -> "result = 31 * Float.hashCode(" ++ fieldName ++ ");\n"
+        M.Double                      -> "result = 31 * Double.hashCode(" ++ fieldName ++ ");\n"
+        M.Boolean                     -> "result = 31 * result + (" ++ fieldName ++ " ? 1 : 0);\n"
         _ | M.isPrimitive fieldType -> "result = 31 * result + " ++ fieldName ++ ";\n"
         _                           -> "result = 31 * result + " ++ fieldName ++ ".hashCode();\n"
       ) fields
     ) ++
-  indent "return result;\n" ++
+  U.indent "return result;\n" ++
   "}\n"
 
 classImports :: J.ClassReader [M.Import]
