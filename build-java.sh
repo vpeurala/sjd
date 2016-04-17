96eab9d228b9f7ec02b918a3e3e2bfd40a336004
Ville Peurala <ville.peurala@gmail.com>
2016-04-17 19:46:12 +0300

    Use options -Xlint and -Werror for javac

--- build-java.sh
+++ build-java.sh
@@ -2,6 +2,5 @@
 stack install
 stack exec sjd-gen test/customer.sjd
 cd src/main/java
-javac -cp /Users/vpeurala/.m2/repository/com/fasterxml/jackson/core/jackson-annotations/2.8.0-SNAPSHOT/jackson-annotations-2.8.0-SNAPSHOT.jar *.java
+javac -Xlint -Werror -cp /Users/vpeurala/.m2/repository/com/fasterxml/jackson/core/jackson-annotations/2.8.0-SNAPSHOT/jackson-annotations-2.8.0-SNAPSHOT.jar *.java
 cd ../../..
-
